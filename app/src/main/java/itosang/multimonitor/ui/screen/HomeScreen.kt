4e38097b7f9c6f17da9a12e91dabe66b1bb33ed9
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2026-04-26 16:29:17 +0800

    refactor: 移除冗余代码

--- app/src/main/java/itosang/multimonitor/ui/screen/HomeScreen.kt
+++ app/src/main/java/itosang/multimonitor/ui/screen/HomeScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import itosang.multimonitor.R
 import itosang.multimonitor.ui.theme.scaffoldInsets
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -21,7 +23,7 @@ fun HomeScreen() {
             topBar = {
                 TopAppBar(
                     title = {
-                        Text("MultiMonitor")
+                        Text(stringResource(R.string.app_name))
                     }
                 )
             }
